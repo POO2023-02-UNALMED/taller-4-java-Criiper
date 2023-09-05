cb51a269a004da79e1ce6ab26655b70b2dd2f14b
Criiper <perezloperacristiandavid@gmail.com>
2023-09-05 10:11:54 -0500

    Commit Personas :bee:
    Se cambia orden de los constructores.

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -10,36 +10,38 @@ public class Persona {
         totalPersonas = 0;
     }
 
-    public Persona(long cedula, String nombre) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
-    }
-    
-    public Persona(String nombre, long cedula) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
-    }
-
-    public Persona(long cedula) {
-        this.cedula = cedula;
-        this.nombre = "";
-        totalPersonas++;
-    }
-
+	public Persona() {
+		this.cedula = 0;
+		this.nombre = "";
+		totalPersonas++;
+	}
+	
     public Persona(String nombre) {
         this.cedula = 1;
     	this.nombre = "";
         totalPersonas++;
     }
     
-
-	public Persona() {
-		this.cedula = 0;
-		this.nombre = "";
-		totalPersonas++;
-	}
+    public Persona(long cedula) {
+        this.cedula = cedula;
+        this.nombre = "";
+        totalPersonas++;
+    }
+    
+    
+    public Persona(String nombre, long cedula) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
+    }
+    
+    public Persona(long cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
+    }
+   
+    
 
 	public long getCedula() {
         return cedula;
